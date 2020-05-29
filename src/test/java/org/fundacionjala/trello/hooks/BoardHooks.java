ffe48cec0c837652d0abdf29c34c83068d797928
Luqp <luceroqp@outlook.com>
2020-05-29 13:11:44 -0400

    Add Board Hook for delete by API.

--- src/test/java/org/fundacionjala/trello/hooks/BoardHooks.java
+++ src/test/java/org/fundacionjala/trello/hooks/BoardHooks.java
@@ -1,20 +1,35 @@
 package org.fundacionjala.trello.hooks;
 
 import io.cucumber.java.After;
+import org.fundacionjala.trello.client.RequestManager;
+import org.fundacionjala.trello.context.Context;
 import org.fundacionjala.trello.pages.board.BoardPage;
 import org.fundacionjala.trello.pages.board.MenuBoard;
 
+import static org.fundacionjala.trello.context.EndPointsEnum.BOARD;
 import static org.fundacionjala.trello.driver.DriverFactory.getChromeDriver;
 
 public final class BoardHooks {
 
     private static final int CLEAN_CONTEXT_ORDER_VALUE_BOARD = 20;
+    private final Context context;
+    private final RequestManager requestManager;
+
+    public BoardHooks(final Context context, final RequestManager requestManager) {
+        this.context = context;
+        this.requestManager = requestManager;
+    }
 
     /**
      * Delete a Board if it was created.
      */
     @After(value = "@deleteBoard", order = CLEAN_CONTEXT_ORDER_VALUE_BOARD)
     public void deleteBoard() {
+        deleteByApi();
+        deleteByUI();
+    }
+
+    private void deleteByUI() {
         BoardPage board = new BoardPage(getChromeDriver());
         MenuBoard menuBoard = new MenuBoard(getChromeDriver());
         if (!board.isDisplayed()) {
@@ -24,5 +39,10 @@ public final class BoardHooks {
             menuBoard = board.displayMenu();
         }
         menuBoard.moreMenuOptions().closeBoard().permanentlyDelete();
+    }
+
+    private void deleteByApi() {
+        context.getIdsByKey(BOARD)
+                .forEach(id -> requestManager.init(context).delete(BOARD.getEndPoint().concat(id)));
     }
 }
