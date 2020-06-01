ee4545c00f4afa8ffe80ea60af3457f962be74b9
Mauricio Oroza <61165905+Snip-77@users.noreply.github.com>
2020-06-01 08:16:50 -0400

    [develop] Mauricio - Create card test (#38)
    * Adding card clases
    
    * Added TDD: create card test
    
    * Adding code for card test
    
    * Fixing code for card test
    
    * Cleaning code
    
    * Cleaning code
    
    * Cleaning code
    
    * Cleaning code
    
    * Cleaning code
    
    * Cleaning code
    
    * Cleaning code
    
    * Refactor
    
    * Cleaning code
    
    * Updating Json Config Dist
    
    * Fixing reviewed comments
    
    * Cleaning code

--- src/main/java/org/fundacionjala/trello/pages/list/ListPage.java
+++ src/main/java/org/fundacionjala/trello/pages/list/ListPage.java
@@ -1,6 +1,9 @@
 package org.fundacionjala.trello.pages.list;
 
+import org.fundacionjala.trello.pages.card.CardForm;
 import org.fundacionjala.trello.pages.core.PageObject;
+import org.fundacionjala.trello.pages.forms.FormPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -11,6 +14,8 @@ import java.util.stream.Collectors;
 public final class ListPage extends PageObject {
 
     private static final String NAME_LISTS_TEXT_AREA = "textarea.list-header-name";
+    public static final String LIST_LOCATOR = "//textarea[contains(text(), '%s')]/parent::div/parent::div/"
+            + "div[contains(@class, 'card-composer-container')]//span[@class= 'icon-sm icon-add']";
 
     @FindBy(css = NAME_LISTS_TEXT_AREA)
     private List<WebElement> listNames;
@@ -31,5 +36,14 @@ public final class ListPage extends PageObject {
 
     public List<String> getAllListsNames() {
         return listNames.stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    private WebElement searchList(final String listName) {
+        return driver.findElement(By.xpath(String.format(LIST_LOCATOR, listName)));
+    }
+
+    public FormPage<?> createNewCard(final String listname) {
+        action.click(searchList(listname));
+        return new CardForm(driver);
     }
 }
