f2a8387f693efe1f061274261b95ed5c874affd7
carlosmc23 <kenshinmc23@gmail.com>
2020-06-03 20:36:50 -0400

    solving merge problems in page object

--- src/main/java/org/fundacionjala/core/ui/pages/WebObject.java
+++ src/main/java/org/fundacionjala/core/ui/pages/WebObject.java
@@ -1,11 +1,8 @@
 package org.fundacionjala.core.ui.pages;
 
-<<<<<<< HEAD:src/main/java/org/fundacionjala/core/ui/pages/WebObject.java
+
+import org.fundacionjala.core.Environment;
 import org.fundacionjala.core.ui.WebDriverAction;
-=======
-import org.fundacionjala.trello.config.Environment;
-import org.fundacionjala.trello.utils.WebDriverAction;
->>>>>>> ad5e39184211e3bc0bc901a3a4b31a5293a96f19:src/main/java/org/fundacionjala/trello/pages/core/WebObject.java
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
