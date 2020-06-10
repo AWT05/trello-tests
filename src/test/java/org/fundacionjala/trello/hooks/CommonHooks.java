3a742b087924702347f57ea0d379a4d9dcf1e3ca
nanrehd <jdhpp_perezotmail.com>
2020-06-09 20:46:46 -0400

    first test for softasserts

--- src/test/java/org/fundacionjala/trello/hooks/CommonHooks.java
+++ src/test/java/org/fundacionjala/trello/hooks/CommonHooks.java
@@ -1,25 +1,35 @@
 package org.fundacionjala.trello.hooks;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.fundacionjala.core.api.RequestManager;
 import org.fundacionjala.trello.context.ContextTrello;
 import org.fundacionjala.trello.context.EndPointsEnum;
+import org.fundacionjala.trello.driver.SharedDriver;
 import org.fundacionjala.trello.pages.menus.Header;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
 
+import static org.fundacionjala.trello.utils.AssertGroup.getAssertGroup;
 import static org.fundacionjala.trello.driver.DriverFactory.getDriver;
+import static org.fundacionjala.trello.utils.AssertGroup.setAssertGroup;
 
 public final class CommonHooks {
 
     private static final int CLEAN_CONTEXT_ORDER = 20;
     private static final int LOGOUT_ORDER = 15;
+    private static final int SOFT_ASSERT_ORDER = 25;
     private final ContextTrello context;
     private final RequestManager requestManager;
     private Header header;
+    private Assertion assertGroup;
 
-    public CommonHooks(final ContextTrello context, final RequestManager requestManager) {
+    public CommonHooks(final SharedDriver sharedDriver, final ContextTrello context,
+                       final RequestManager requestManager) {
         this.context = context;
         this.requestManager = requestManager;
         header = new Header(getDriver());
+        this.assertGroup = getAssertGroup();
     }
 
     /**
@@ -40,5 +50,22 @@ public final class CommonHooks {
     @After(order = LOGOUT_ORDER)
     public void logoutAccount() {
         header.getMenuProfile().logOutSession();
+    }
+
+    /**
+     * Initialize a SoftAssert instead of HardAssert for a test.
+     */
+    @Before(value = "@softAssert")
+    public void initializeSoftAssert() {
+        assertGroup = new SoftAssert();
+        setAssertGroup(assertGroup);
+    }
+
+    /**
+     * Assert all assertions stored from a test.
+     */
+    @After(value = "@softAssert", order = SOFT_ASSERT_ORDER)
+    public void assertAll() {
+        ((SoftAssert) assertGroup).assertAll();
     }
 }
--- src/test/java/org/fundacionjala/trello/stepdefs/TeamStepDefs.java
+++ src/test/java/org/fundacionjala/trello/stepdefs/TeamStepDefs.java
@@ -8,21 +8,25 @@ import org.fundacionjala.trello.driver.SharedDriver;
 import org.fundacionjala.trello.pages.team.TeamInviteForm;
 import org.fundacionjala.trello.pages.team.TeamPage;
 
+import org.testng.asserts.Assertion;
+
 import java.util.Map;
 
+import static org.fundacionjala.trello.utils.AssertGroup.getAssertGroup;
 import static org.fundacionjala.trello.driver.DriverFactory.getDriver;
-import static org.testng.Assert.assertEquals;
 
 public final class TeamStepDefs {
 
     private Context context;
     private TeamPage teamPage;
     private TeamInviteForm teamInviteForm;
+    private Assertion assertGroup;
 
     public TeamStepDefs(final SharedDriver sharedDriver, final Context context) {
         this.context = context;
         this.teamPage = new TeamPage(getDriver());
         this.teamInviteForm = new TeamInviteForm(getDriver());
+        this.assertGroup = getAssertGroup();
     }
 
     /**
@@ -32,7 +36,7 @@ public final class TeamStepDefs {
      */
     @Then("I should have a team created with the following data")
     public void validateCreationWithData(final Map<String, String> expectedData) {
-        assertEquals(teamPage.getTeamName(), expectedData.get("name"));
+        assertGroup.assertEquals(teamPage.getTeamName(), expectedData.get("name"));
     }
 
     /**
