bf567ffbd5940954ea91f1efb67feb078f04fe32
Snip-77 <mau.oroza1@gmail.com>
2020-06-10 04:11:00 -0400

    Added first raw hardcoded mobile test

--- src/test/java/org/fundacionjala/trello/Mobile/CreateBoardMobileTest.java
+++ src/test/java/org/fundacionjala/trello/Mobile/CreateBoardMobileTest.java
@@ -3,11 +3,14 @@ package org.fundacionjala.trello.Mobile;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,6 +24,7 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class CreateBoardMobileTest {
     protected static WebDriver driver;
+    protected WebDriverWait wait;
     private final static String ADD_BUTTON = "com.trello:id/add_fab";
     private final static String ADD_BOARD_BUTTON = "com.trello:id/add_board_fab";
     private final static String BOARD_NAME = "com.trello:id/board_name";
@@ -28,10 +32,15 @@ public class CreateBoardMobileTest {
     private final static String LIST_OF_TEAMS_XPATH = "//android.widget.ListView/android.view.ViewGroup";
     private final static String CREATE_BUTTON = "com.trello:id/create_board";
     private final static String BOARD_TITLE = "com.trello:id/toolbar_title";
+    private final static String PRIMARY_CREDENTIAL = "com.google.android.gms:id/credential_primary_label";
+    private final static String LIST_OF_HOME_XPATH = "//androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout";
 
+    private final static String ADD_BUTTON_XPATH = "//android.widget.ImageButton[@content-desc='Open Create Board or Add Card menu']";
+    @FindBy(xpath = ADD_BUTTON_XPATH)
+    private MobileElement addButtonXpath;
 
     @FindBy(id = ADD_BUTTON)
-    private MobileElement addButton;
+    private WebElement addButton;
 
     @FindBy(id = ADD_BOARD_BUTTON)
     private MobileElement addCardButton;
@@ -51,8 +60,15 @@ public class CreateBoardMobileTest {
     @FindBy(id = BOARD_TITLE)
     private MobileElement boardTitle;
 
+    @FindBy(id = PRIMARY_CREDENTIAL)
+    private MobileElement primarySmartlockCredential;
+
+    @FindBy(xpath = LIST_OF_HOME_XPATH)
+    private List<MobileElement> listOfAllElements;
+
     @BeforeMethod
     public static void setUp() {
+
         DesiredCapabilities desCap = new DesiredCapabilities();
         desCap.setCapability("deviceName", "Mau-android");
         desCap.setCapability("platformName", "Android");
@@ -71,7 +87,6 @@ public class CreateBoardMobileTest {
         }
 
 
-
 //        AndroidDriver driver2=new AndroidDriver(desCap);
         System.out.println("Executed BEFORE method");
     }
@@ -84,8 +99,14 @@ public class CreateBoardMobileTest {
 
     @Test
     public void createBoardMobileTest() {
+        wait = new WebDriverWait(driver, 15);
         //login
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.trello:id/view_pager")));
+//        primarySmartlockCredential.click();
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout")));
         addButton.click();
+//        driver.findElement(By.id("com.trello:id/add_fab")).click();
+        addButtonXpath.click();
         addCardButton.click();
         boardName.sendKeys("FirstMobileBoard");
         teamDropdown.click();
